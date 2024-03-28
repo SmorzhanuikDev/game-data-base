7ba9494652cc1a27fdeb04e41ac5532e7bf94d30
Smorzhaniuk <smorzhaniuk.cinis@gmail.com>
2024-03-28 18:30:34 +0200

    first steps
    install and test scss,
    install axios and test api
    delete useless file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import s from './main.module.scss'
+import {instance} from "./API";
+import axios from "axios";
 function App() {
+
+  // const onClick = () => {
+  //   instance.get('platforms').then(
+  //       function (res:any) {
+  //         console.log(res)
+  //       }
+  //   )
+  //       .catch(function (error:any) {
+  //         console.log(error)
+  //       })
+  // }
+    const token = 'de12c3781cbf437fb959b333da49c533'
+  const onClick = () => {
+    axios.get('https://api.rawg.io/api/platforms?key=de12c3781cbf437fb959b333da49c533').then(
+        function (res:any) {
+          console.log(res)
+        }
+    )
+        .catch(function (error:any) {
+          console.log(error)
+        })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={s.test}>some text
+        <button onClick={()=> onClick()}>click</button>
     </div>
   );
 }
