e078a7e7e882a4f667220f29760cb6e96df638ce
Smorzhaniuk <smorzhaniuk.cinis@gmail.com>
2024-04-01 20:57:27 +0300

    minor UI update

--- src/Pages/Games/Components/GameItem.tsx
+++ src/Pages/Games/Components/GameItem.tsx
@@ -1,36 +1,33 @@
 import React from 'react';
-import onImage from '../../../Images/no-image.png'
 import s from '../Games.module.scss'
 import {gameType} from "../gamesTypes";
-import {platform} from "node:os";
-import {useNavigate} from "react-router-dom";
 
 interface props {
     game: gameType
 }
 
-export const GameItem:React.FC<props> = ({game}) => {
+export const GameItem: React.FC<props> = ({game}) => {
 
-    const navigate = useNavigate()
 
     return (
         <div className={s.gameItem}>
-            <img onClick={() => navigate('games')} src={game.background_image} alt='gameImage'/>
+            <a href={`game/${game.id}`}>
+                <img src={game.background_image} alt='gameImage'/>
+            </a>
             <div className={s.descBlock}>
                 <div>
-                    <a href={'games'} className={s.gameName}>{game.name}</a>
+                    <a href={`game/${game.id}`} className={s.gameName}>{game.name}</a>
                 </div>
                 <div className={s.gameGenre}>
-                    {game.genres.map(genre => <a href={'games'} key={genre.id}>{genre.name}</a> )   }
+                    {game.genres.map(genre => <a href={'games'} key={genre.id}>{genre.name}</a>)}
                 </div>
                 <div className={s.gamePlatforms}>
                     {game.platforms.map(({platform}) => <a href={'games'} key={platform.id}>
                         {platform.name}
                     </a>)}
                 </div>
-
-                <p>{game.released}</p>
-                <p>5/{game.rating}</p>
+                <p className={s.gameRelease}>Released: {game.released}</p>
+                <p className={s.gameRating}>5/{game.rating}</p>
             </div>
         </div>
     );
