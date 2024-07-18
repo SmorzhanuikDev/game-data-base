0f44504e745c22a980f5ff01436513c2ba4d74ec
Smorzhaniuk <smorzhaniuk.cinis@gmail.com>
2024-07-18 15:59:33 +0300

    made some layout for tags functionality

--- src/Pages/Games/Components/Filters.tsx
+++ src/Pages/Games/Components/Filters.tsx
@@ -4,6 +4,7 @@ import {Select} from "../../../Common/Components/Select/Select";
 import {BiSortAlt2} from "react-icons/bi";
 import {orderOptions, platformsOptions, releasedOptions} from "../selectData";
 import {useSearchParams} from "react-router-dom";
+import { IoCloseOutline } from "react-icons/io5";
 import {IoIosCloseCircle} from "react-icons/io";
 
 interface props {
@@ -37,34 +38,53 @@ export const Filters: React.FC<props> = (props) => {
     }
 
     return (
-        <div className={s.filterBlock}>
-            <div className={s.orderingSection}>
-                <Select options={orderOptions} title={'Order by'} onChangeSelect={setOrder} value={order}/>
-                <BiSortAlt2 className={s.sortButton} onClick={order ? () => setIsReversed(!isReversed) : () => {
-                }}
-                            style={order
-                                ? {
-                                    background: isReversed ? '#f4f4f4' : '#292927',
-                                    color: isReversed ? 'black' : 'white'
-                                }
-                                : {background: '#706f6f', color: '#fff', cursor: 'auto'}
-                            }/>
-            </div>
-            <Select title={'platform'} options={platformsOptions} onChangeSelect={setPlatforms} value={platforms}/>
-            <Select title={'Released'} options={releasedOptions()} value={dates}
-                    onChangeSelect={setDates}/>
-            {
-                search
-                    ? <div className={s.deleteSearch} onClick={cleanSearch}>
-                        <div>
-                            <span>{'Search: '}</span>
-                            <span className={s.searchContent}>{search}</span>
-                        </div>
+        <div>
+            <div className={s.filterBlock}>
+                <div className={s.orderingSection}>
+                    <Select options={orderOptions} title={'Order by'} onChangeSelect={setOrder} value={order}/>
+                    <BiSortAlt2 className={s.sortButton} onClick={order ? () => setIsReversed(!isReversed) : () => {
+                    }}
+                                style={order
+                                    ? {
+                                        background: isReversed ? '#f4f4f4' : '#292927',
+                                        color: isReversed ? 'black' : 'white'
+                                    }
+                                    : {background: '#706f6f', color: '#fff', cursor: 'auto'}
+                                }/>
+                </div>
+                <Select title={'platform'} options={platformsOptions} onChangeSelect={setPlatforms} value={platforms}/>
+                <Select title={'Released'} options={releasedOptions()} value={dates}
+                        onChangeSelect={setDates}/>
+                {
+                    search
+                        ? <div className={s.deleteSearch} onClick={cleanSearch}>
+                            <div>
+                                <span>{'Search: '}</span>
+                                <span className={s.searchContent}>{search}</span>
+                            </div>
 
-                        <IoIosCloseCircle className={s.icon}/>
+                            <IoIosCloseCircle className={s.icon}/>
+                        </div>
+                        : null
+                }
+            </div>
+            <div className={s.tagsBlock}>
+                Tags:
+                <div className={s.tagsList}>
+                    <span className={s.tag}>tag</span>
+                    <span className={s.tag}>tag</span>
+                    <span className={s.tag}>tag</span>
+                </div>
+                <div className={s.tagsModal}>
+                    <div className={s.tagModalHead}>
+                        <span>Select tags</span>
+                        <IoCloseOutline className={s.closeIcon}/>
                     </div>
-                    : null
-            }
+                    <span className={s.tag}>tag</span>
+                    <span className={s.tag}>tag</span>
+                    <span className={s.tag}>tag</span>
+                </div>
+            </div>
         </div>
     );
 };
