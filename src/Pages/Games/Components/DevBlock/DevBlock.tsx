3736a5308e59adafebf164ec0961956e25652121
Smorzhaniuk <smorzhaniuk.cinis@gmail.com>
2024-12-18 17:01:27 +0200

    made developer block
    made UI and logic for developer button

--- src/Pages/Games/Components/DevBlock/DevBlock.tsx
+++ src/Pages/Games/Components/DevBlock/DevBlock.tsx
@@ -1,30 +1,45 @@
 import React, {FC, useEffect} from 'react';
-import {useSearchParams} from "react-router-dom";
 import {useAppDispatch, useAppSelector} from "../../../../hooks";
-import {setIsAppLoading} from "../../../../appSlice";
-import SmallLoader from "../../../../Common/Components/SmallLoader";
 import {fetchDeveloperAction} from "../../gamesSaga";
+import {IoClose} from "react-icons/io5";
+import s from './devBlock.module.scss'
+import {useSearchParams} from "react-router-dom";
+
 
 interface props {
     devId: string | undefined;
 }
 
-export const DevBlock:FC<props> = ({devId}) => {
+export const DevBlock: FC<props> = ({devId}) => {
 
     const dispatch = useAppDispatch();
     const developer = useAppSelector(state => state.games.developer);
-    
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const removeDev = () => {
+        searchParams.delete('dev')
+        setSearchParams(searchParams)
+    }
+
     useEffect(() => {
         if (devId) {
             dispatch(fetchDeveloperAction(Number(devId)))
         }
     }, [devId, dispatch]);
 
-        return (
+    if (!devId) return null;
+    return (
+        <div className={s.devButton} onClick={removeDev}>
             <div>
-                {developer.name}
+                <span className={s.beforeTitle}>Games developed by:</span>
+                <span className={s.title}>{developer.name}</span>
             </div>
-        );
+            <div className={s.closeButton}>
+                <span className={s.closeText}>Remove developer</span>
+                <IoClose className={s.closeIcon}/>
+            </div>
+        </div>
+    );
 
 };
 
