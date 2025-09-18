bc0c1d060e4076c4fbf09fd2303b8bf98a610e18
Smorzhaniuk <smorzhaniuk.cinis@gmail.com>
2025-09-18 18:05:31 +0300

    destructuring opportunities component

--- src/Pages/Authorization/Auth.tsx
+++ src/Pages/Authorization/Auth.tsx
@@ -1,37 +1,17 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import s from './Auth.module.scss'
 import {useBgImage} from "../../Surface/Content";
 import profileBGImage from '../../Images/profileBGImage.jpg'
-import descImage from '../../Images/authDescImage.jpg'
-import {IoEyeOffOutline} from "react-icons/io5";
-import {IoEyeOutline} from "react-icons/io5";
+import {IoEyeOffOutline, IoEyeOutline} from "react-icons/io5";
+import {Opportunities} from "./Components/Opportunities/Opportunities";
 
 
 export const Auth = () => {
 
     const {sendImage} = useBgImage()
-    const descBlock = useRef<HTMLDivElement>(null)
-    const singUp = useRef<HTMLDivElement>(null)
-    const logIn = useRef<HTMLDivElement>(null)
-    const [activeOption, setActiveOption] = useState<'singUp' | 'logIn'>('logIn')
     const [passwordType, setPasswordType] = useState<'text' | 'password'>('password')
 
-    const changeAuthOption = () => {
-        if (descBlock.current && singUp.current && logIn.current) {
-            if (activeOption === 'logIn') {
-                descBlock.current.style.right = '-20%'
-                singUp.current.style.left = '1px'
-                logIn.current.style.left = '100vw'
-                setActiveOption('singUp')
-            } else {
-                setActiveOption('logIn')
-                descBlock.current.style.right = '-195%'
-                singUp.current.style.left = '-100vw'
-                logIn.current.style.left = '1px'
 
-            }
-        }
-    }
     const changePassType = () => {
         if (passwordType === 'password') {
             setPasswordType('text')
@@ -47,30 +27,7 @@ export const Auth = () => {
     return (
         <div className={s.auth}>
             <div className={s.singUp}>
-                <div className={s.description} ref={descBlock}>
-                    <div className={s.descText} ref={singUp}>
-                        <h3>Authorization allow you:</h3>
-                        <div className={s.listItem}>&#x2022; Create lists of games for any needs</div>
-                        <div className={s.listItem}>&#x2022; Rate games for share your impressions</div>
-                        <div className={s.listItem}>&#x2022; Get access to your lists and rates on other device</div>
-                        <div className={s.switchOption}>
-                            <span className={s.text}>Have an account?&nbsp;</span>
-                            <span className={s.link} onClick={changeAuthOption}>Log in</span>
-                        </div>
-                    </div>
-                    <div className={s.descText2} ref={logIn}>
-                        <h3>Authorization allow you:</h3>
-                        <div className={s.listItem}>&#x2022; Create lists of games for any needs</div>
-                        <div className={s.listItem}>&#x2022; Rate games for share your impressions</div>
-                        <div className={s.listItem}>&#x2022; Get access to your lists and rates on other device</div>
-                        <div className={s.switchOption}>
-                            <span className={s.text}>Don't have an account?&nbsp;</span>
-                            <span className={s.link} onClick={changeAuthOption}>Sing up</span>
-                        </div>
-                    </div>
-                    <img className={s.image} src={descImage} alt="desc"/>
-                    <div className={s.gradient}/>
-                </div>
+                <Opportunities/>
                 <span className={s.title}>
                     Log in
                 </span>
---
+++ src/Pages/Authorization/Components/Opportunities/Opportunities.tsx
@@ -0,0 +1,56 @@
+import React, {useRef, useState} from 'react';
+import s from "./opportunities.module.scss";
+import descImage from "../../../../Images/authDescImage.jpg";
+
+
+
+export const Opportunities: React.FC = () => {
+
+    const opportunitiesRef = useRef<HTMLDivElement>(null)
+    const singUp = useRef<HTMLDivElement>(null)
+    const logIn = useRef<HTMLDivElement>(null)
+    const [activeOption, setActiveOption] = useState<'singUp' | 'logIn'>('logIn')
+
+    const changeAuthOption = () => {
+        if (opportunitiesRef.current && singUp.current && logIn.current) {
+            if (activeOption === 'logIn') {
+                opportunitiesRef.current.style.right = '-20%'
+                singUp.current.style.left = '1px'
+                logIn.current.style.left = '100vw'
+                setActiveOption('singUp')
+            } else {
+                setActiveOption('logIn')
+                opportunitiesRef.current.style.right = '-195%'
+                singUp.current.style.left = '-100vw'
+                logIn.current.style.left = '1px'
+
+            }
+        }
+    }
+    return (
+        <div className={s.opportunities} ref={opportunitiesRef}>
+            <div className={s.logInText} ref={singUp}>
+                <h3>Authorization allow you:</h3>
+                <div className={s.listItem}>&#x2022; Create lists of games for any needs</div>
+                <div className={s.listItem}>&#x2022; Rate games for share your impressions</div>
+                <div className={s.listItem}>&#x2022; Get access to your lists and rates on other device</div>
+                <div className={s.switchOption}>
+                    <span className={s.text}>Have an account?&nbsp;</span>
+                    <span className={s.link} onClick={changeAuthOption}>Log in</span>
+                </div>
+            </div>
+            <div className={s.SingInText} ref={logIn}>
+                <h3>Authorization allow you:</h3>
+                <div className={s.listItem}>&#x2022; Create lists of games for any needs</div>
+                <div className={s.listItem}>&#x2022; Rate games for share your impressions</div>
+                <div className={s.listItem}>&#x2022; Get access to your lists and rates on other device</div>
+                <div className={s.switchOption}>
+                    <span className={s.text}>Don't have an account?&nbsp;</span>
+                    <span className={s.link} onClick={changeAuthOption}>Sing up</span>
+                </div>
+            </div>
+            <img className={s.image} src={descImage} alt="desc"/>
+            <div className={s.gradient}/>
+        </div>
+    );
+};
