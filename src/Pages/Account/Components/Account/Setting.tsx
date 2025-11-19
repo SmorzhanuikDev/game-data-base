b0f55efdb1a455420aeb3a62d9a4d61b16222f41
Smorzhaniuk <smorzhaniuk.cinis@gmail.com>
2025-11-19 14:09:49 +0200

    update account setting UI

--- src/Pages/Account/Components/Account/Setting.tsx
+++ src/Pages/Account/Components/Account/Setting.tsx
@@ -4,6 +4,8 @@ import {useAppDispatch, useAppSelector} from "../../../../hooks";
 import {useNavigate} from "react-router-dom";
 import {accountAction} from "../../AccountSaga";
 import s from './account.module.scss'
+import {CiWarning} from "react-icons/ci";
+
 
 export const Setting = () => {
 
@@ -45,17 +47,25 @@ export const Setting = () => {
             </div>
             <div className={s.profileBox}>
                 <span className={s.desc}>Change name</span>
-                <input type="text"/>
+                <input type="text" className={s.profileInput}/>
                 <button className={s.profileButton}>Change</button>
             </div>
             <div className={s.profileBox}>
                 <span className={s.desc}>Change password</span>
-                <input type="text"/>
-                <input type="text"/>
-                <input type="text"/>
+                <input className={s.profileInput} type="text"/>
+                <input className={s.profileInput} type="text"/>
+                <input className={s.profileInput} type="text"/>
                 <button className={s.profileButton}>Change</button>
             </div>
-            <button className={s.profileButton}>Delete account</button>
+            <div className={s.profileBox}>
+                <div className={s.deleteInfo}>
+                    <CiWarning className={s.warningIcon}/>
+                    <span>
+                        Once deleted, your game lists and ratings cannot be restored.
+                    </span>
+                </div>
+                <button className={s.profileButton}>Delete account</button>
+            </div>
         </div>
     );
 };
